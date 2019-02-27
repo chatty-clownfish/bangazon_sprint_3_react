8b2954c57f1224afd494aa0b6866249658cc811c
buffard <samuelwebber19@gmail.com>
2019-02-27 15:57:31 -0600

    list of training programs show up

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -68,7 +68,13 @@ export default class ApplicationViews extends Component {
             <Route exact path="/producttypes" render={(props) => { return <ProductTypes {...props}/> }} />
             <Route exact path="/customers" render={(props) => { return <Customers {...props}/> }} />
             <Route exact path="/products" render={(props) => { return <Products {...props}/> }} />
-            <Route exact path="/trainings" render={(props) => { return <Trainings {...props}/> }} />
+            
+            <Route exact path="/trainings" render={(props) => {
+                return <Trainings {...props}
+                training={this.state.training}
+                getAll={this.getAll}
+                /> }} />
+                
             <Route exact path="/computers" render={(props) => { return <Computers {...props}/> }} />
             <Route exact path="/departments" render={(props) => { return <Departments department={this.state.department} {...props} getAll={this.getAll}/> }} />
             <Route exact path="/paymenttypes" render={(props) => { return <PaymentTypes {...props}/> }} />
